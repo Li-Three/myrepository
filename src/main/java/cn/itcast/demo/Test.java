5616ffd506aed4a3a4d92c69279e54314ebffe7a
ljq <Lijia_qing@163.com>
2018-07-23 19:51:16 +0800

    cllo

--- src/main/java/cn/itcast/demo/Test.java
+++ src/main/java/cn/itcast/demo/Test.java
@@ -9,7 +9,9 @@ package cn.itcast.demo;
 public class Test {
 
 	public static void main(String[] args) {
+
 		System.out.println("========1111========");
+		System.out.println("========2222========");
 	}
 
 }
