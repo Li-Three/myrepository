55044a622773c41021876f6f113fac3cd54b4dd6
ljq <Lijia_qing@163.com>
2018-07-23 20:18:38 +0800

    tow66

--- src/main/java/cn/itcast/demo/Test.java
+++ src/main/java/cn/itcast/demo/Test.java
@@ -13,6 +13,7 @@ public class Test {
 		System.out.println("========1111========");
 		System.out.println("========2222========");
 		System.out.println("========3333========");
+		System.out.println("========777=========");
 	}
 
 }
